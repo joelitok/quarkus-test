f2fb133fb6e5c3635faff4998c25a82542ec378d
joelitok <joelnkouatchet@gmail.com>
2022-06-23 08:37:12 +0100

    add information

--- src/main/java/org/tetv/GreetingResource.java
+++ src/main/java/org/tetv/GreetingResource.java
@@ -1,7 +1,8 @@
 package org.tetv;
 
 import java.util.List;
-
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PUT;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,42 +13,83 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.transaction.Transactional;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+
 
 @Path("/episodes")
+@ApplicationScoped
+@Produces("application/json")
+@Consumes("application/json")
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    //@Produces(MediaType.APPLICATION_JSON)
     public List<Episode> episodes() {
         return Episode.listAll();
     }
 
     @GET
-    @Path("/{episode}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Episode> findEpisode(@PathParam("episode") String episode) {
-        if(episode!=null){
-            return Episode.find("title", episode).list();
+    @Path("/{episodeTitle}")
+   // @Produces(MediaType.APPLICATION_JSON)
+    public List<Episode> findEpisode(@PathParam("episodeTitle") String episodeTitle) {
+        if(episodeTitle!=null){
+            return Episode.find("title", episodeTitle).list();
         }
             return Episode.listAll();
     }
 
+
     @GET
-    @Path("/{episodeId}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String deleteEpisode(@PathParam("episodeId") Long episode){
-        if(episode!=null){
-            Episode.deleteById(episode);
-            return "episode supprimer";
-        }else{
-            return "cette episode n existe pas";
+    @Path("/find/{episodeId}")
+    public Episode getSingleEpisode(@PathParam Long episodeId) {
+        Episode entity = Episode.findById(episodeId);
+        if (entity == null) {
+            throw new WebApplicationException("Episode with id of " + episodeId + " does not exist..", 404);
         }
-        
+           return entity;
     }
 
+
+    @PUT
+    @Path("/update/{episodeId}")
+    @Transactional
+    public Episode updateEpisode(@PathParam Long episodeId, Episode episode) {
+        if (episode.title == null) {
+            throw new WebApplicationException("episode title was not set on request.", 422);
+            }
+
+        Episode entity = Episode.findById(episodeId);
+        if (entity == null) {
+            throw new WebApplicationException("episode with id of " + episodeId + " does not exist.", 404);
+        }
+        entity.title = episode.title;
+        entity.description =episode.description;
+        return entity;
+    }
+
+
+    
+    @DELETE
+    @Path("/delete/{episodeId}")
+    @Transactional
+    public Response deleteEpisode(@PathParam Long episodeId) {
+        Episode entity = Episode.findById(episodeId);
+        if (entity == null) {
+            throw new WebApplicationException("Episode with id of " + episodeId + " does not exist.", 404);
+        }
+        entity.delete();
+        return Response.status(204).build();
+    }
+
+
+
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
+    //@Produces(MediaType.APPLICATION_JSON)
+   // @Consumes(MediaType.APPLICATION_JSON)
     @Transactional
     public Response saveEpisode(Episode episode){
         episode.persist();
@@ -57,6 +99,20 @@ public class GreetingResource {
 
 
 
+
+    @Provider
+    public static class ErrorMapper implements ExceptionMapper<Exception> {
+        @Override
+        public Response toResponse(Exception exception) {
+            int code = 500;
+            if (exception instanceof WebApplicationException) {
+                code = ((WebApplicationException) exception).getResponse().getStatus();
+            }
+        return Response.status(code)
+                    .entity(Json.createObjectBuilder().add("error", exception.getMessage()).add("code", code).build())
+                    .build();
+        }
+    }
     
 
 
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO Episode(id, title, description) VALUES (nextval('hibernate_sequence'),'e1','first episode');
+INSERT INTO Episode(id, title, description) VALUES (nextval('hibernate_sequence'),'e2','two episode');
+INSERT INTO Episode(id, title, description) VALUES (nextval('hibernate_sequence'),'e3','three episode');
+INSERT INTO Episode(id, title, description) VALUES (nextval('hibernate_sequence'),'e4','four episode');
+INSERT INTO Episode(id, title, description) VALUES (nextval('hibernate_sequence'),'e5','five episode');
